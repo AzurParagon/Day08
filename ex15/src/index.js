9eb5102688e3d255d29d2bf86120f94506edb971
Azur Ramic <azurramic@gmail.com>
2021-03-16 10:19:33 +0100

    Add finished task

---
+++ ex15/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function convertToInt(str) {
+    var a = parseInt(str, 16);
+    return a;
+}
+convertToInt("JeffBezos");
+// Only change code above this line
+module.exports = convertToInt;
